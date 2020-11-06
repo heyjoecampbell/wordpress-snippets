eecae576b6c1e75d6f15bd45820ffc678039db90
Joe Campbell <mrjosephcampbell@gmail.com>
2020-11-06 08:55:39 -0500

    Update and rename wp-boilerplate-site-specific-plugin to wp-boilerplate-site-specific-plugin.php

--- wp-boilerplate-site-specific-plugin.php
+++ wp-boilerplate-site-specific-plugin.php
@@ -1,4 +1,6 @@
 <?php
+/********* DO NOT COPY THE PARTS ABOVE THIS LINE *********/
+
 /*
 Plugin Name: _Add Title WITH UNDERSCORE at beginning so its listed first in plugin list
 Description: Add a description - I use: Collection of code snippets for creating an awesome WordPress website
@@ -10,4 +12,3 @@ Author: Add your name with or without link - I use: <a href="https://twitter.com
 
 
 // THE END
-?>
