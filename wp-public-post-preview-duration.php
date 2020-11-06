19246e4aa80b3f177e8af52d48540e3582d5a792
Joe Campbell <mrjosephcampbell@gmail.com>
2020-11-06 08:58:20 -0500

    Update and rename wp-public-post-preview-duration.txt to wp-public-post-preview-duration.php

--- wp-public-post-preview-duration.php
+++ wp-public-post-preview-duration.php
@@ -1,3 +1,6 @@
+<?php
+/********* DO NOT COPY THE PARTS ABOVE THIS LINE *********/
+
 /*
 Draft Preview URL Duration
 source: https://wordpress.org/plugins/public-post-preview/#48%20hours%20are%20not%20enough%20to%20me.%20can%20i%20extend%20the%20nonce%20time%3F
